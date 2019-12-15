ef037780815f3fb8d1e0a889c676583664cb39c2
liuyu2012 <liuyu2012@yeah.net>
2019-12-15 21:53:17 +0800

    添加 数据源配置

--- mango/src/main/java/com/liuyu/mango/MangoApplication.java
+++ mango/src/main/java/com/liuyu/mango/MangoApplication.java
@@ -3,7 +3,10 @@ package com.liuyu.mango;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * @author Administrator
+ */
+@SpringBootApplication(scanBasePackages = {"com.liuyu.mango"})
 public class MangoApplication {
 
     public static void main(String[] args) {
