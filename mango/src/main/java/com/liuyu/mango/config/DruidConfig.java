8923ce0d5196bf85d6f648c90780f90de68f46fe
liuyu2012 <liuyu2012@yeah.net>
2019-12-19 13:40:47 +0800

    spring boot 整合 druid 重新配置

--- mango/src/main/java/com/liuyu/mango/config/DruidConfig.java
+++ mango/src/main/java/com/liuyu/mango/config/DruidConfig.java
@@ -3,20 +3,14 @@ package com.liuyu.mango.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
-import com.liuyu.mango.properties.DruidDataSourceProperties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.google.common.collect.Maps;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Resource;
 import javax.servlet.Servlet;
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -27,82 +21,46 @@ import java.util.Map;
  * @version: 1.0
  */
 @Configuration
-@EnableConfigurationProperties({DruidDataSourceProperties.class})
 public class DruidConfig {
 
-    @Resource
-    private DruidDataSourceProperties properties;
-
-
+    @ConfigurationProperties(prefix = "spring.datasource")
     @Bean
-    @ConditionalOnMissingBean
-    public DataSource druid() {
-
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setDriverClassName(properties.getDriverClassName());
-        druidDataSource.setUrl(properties.getUrl());
-        druidDataSource.setUsername(properties.getUsername());
-        druidDataSource.setPassword(properties.getPassword());
-        druidDataSource.setInitialSize(properties.getInitialSize());
-        druidDataSource.setMinIdle(properties.getMinIdle());
-        druidDataSource.setMaxActive(properties.getMaxActive());
-        druidDataSource.setMaxWait(properties.getMaxWait());
-        druidDataSource.setTimeBetweenEvictionRunsMillis(properties.getTimeBetweenEvictionRunsMillis());
-        druidDataSource.setMinEvictableIdleTimeMillis(properties.getMinEvictableIdleTimeMillis());
-        druidDataSource.setValidationQuery(properties.getValidationQuery());
-        druidDataSource.setTestWhileIdle(properties.isTestWhileIdle());
-        druidDataSource.setTestOnBorrow(properties.isTestOnBorrow());
-        druidDataSource.setTestOnReturn(properties.isTestOnReturn());
-        druidDataSource.setPoolPreparedStatements(properties.isPoolPreparedStatements());
-        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(properties.getMaxPoolPreparedStatementPerConnectionSize());
-
-
-        try {
-//            druidDataSource.setFilters(properties.getFilters());
-            druidDataSource.init();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return druidDataSource;
+    public DruidDataSource druidDataSource(){
+        return new DruidDataSource();
     }
 
-
     @Bean
-    @ConditionalOnMissingBean
-    public ServletRegistrationBean<Servlet> druidServlet() {
+    public ServletRegistrationBean<Servlet> statViewServlet(){
         ServletRegistrationBean<Servlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        Map<String,String> initParams = Maps.newHashMap();
 
+        // 登录查看信息的账号和密码
+        initParams.put("loginUsername","admin");
+        initParams.put("loginPassword", "admin");
         // 白名单
-        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        initParams.put("allow", "");//默认就是允许所有访问
         // 黑名单
-        servletRegistrationBean.addInitParameter("deny", "192.168.1.119");
-
-        // 登录查看信息的账号密码，用于登录 Druid 监控后台
-        servletRegistrationBean.addInitParameter("loginUsername", "admin");
-        servletRegistrationBean.addInitParameter("loginPassword", "admin");
-
+        initParams.put("deny", "192.168.1.1");
         // 是否能够重置数据
+        initParams.put("resetEnable","false");
 
-
+        servletRegistrationBean.setInitParameters(initParams);
         return servletRegistrationBean;
     }
 
 
     @Bean
-    @ConditionalOnMissingBean
-    public FilterRegistrationBean webStatFilter() {
-        FilterRegistrationBean bean = new FilterRegistrationBean();
-        bean.setFilter(new WebStatFilter());
-
-        Map<String, String> initParams = new HashMap<>();
-        initParams.put("exclusions", "*.js,*.css,/druid/*");
-
-        bean.setInitParameters(initParams);
-
-        bean.setUrlPatterns(Arrays.asList("/*"));
-
-        return bean;
+    public FilterRegistrationBean<WebStatFilter> webStatFilter() {
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions",  "*.js,*.css,/druid/*");
+        return filterRegistrationBean;
     }
+
+
+
+
+
 
 }
