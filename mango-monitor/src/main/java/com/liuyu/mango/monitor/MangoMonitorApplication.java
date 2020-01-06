263062abe089f538997380f229867c65cf097160
liuyu2012 <liuyu2012@yeah.net>
2020-01-06 14:01:35 +0800

    代码调试

--- mango-monitor/src/main/java/com/liuyu/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/liuyu/mango/monitor/MangoMonitorApplication.java
@@ -1,5 +1,6 @@
 package com.liuyu.mango.monitor;
 
+
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author liu yu
  */
 @EnableAdminServer
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class MangoMonitorApplication {
 
--- mango-monitor/src/test/java/com/liuyu/mango/monitor/MangoMonitorApplicationTests.java
+++ mango-monitor/src/test/java/com/liuyu/mango/monitor/MangoMonitorApplicationTests.java
@@ -1,13 +1,8 @@
 package com.liuyu.mango.monitor;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MangoMonitorApplicationTests {
+public class MangoMonitorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
