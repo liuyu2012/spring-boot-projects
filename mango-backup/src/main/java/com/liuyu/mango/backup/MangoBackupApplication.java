6f06163e2d1c5a605322754c90b0b5d7b7da874d
liuyu2012 <liuyu2012@yeah.net>
2020-01-07 15:54:56 +0800

    备份平台添加服务发现

--- mango-backup/src/main/java/com/liuyu/mango/backup/MangoBackupApplication.java
+++ mango-backup/src/main/java/com/liuyu/mango/backup/MangoBackupApplication.java
@@ -2,6 +2,7 @@ package com.liuyu.mango.backup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author： yu Liu
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @modifiedBy：
  * @version: 1.0
  */
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.liuyu.mango"})
 public class MangoBackupApplication {
 
